97e10b23199878bc468ec7e8028a837085f0ca07
Khawaja Farooq <kfarooqa@gmail.com>
2017-07-01 13:27:17 +0500

    new resources

--- MLTest-CoreML/ViewController.swift
+++ MLTest-CoreML/ViewController.swift
@@ -11,28 +11,26 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var image: UIImageView!
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
-    let interval = 2.5
+    // ivars
     var i = 0
+    var predictionResult: [Prediction] = []
     
     // ML model
     let model = Inceptionv3()
     
     // images to predict
-    let images = ["img1.png","img2.png","img3.png","img4.png","img5.png","img6.png"]
+    let images = ["img1.png","img2.png","img3.png","img4.png","img5.png"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.image.image = nil
-        self.label.text = ""
+        self.headerLabel.text = ""
         
-        // timer to repeatedly call changeImage
-        Timer.scheduledTimer(timeInterval: interval, target: self,
-                             selector: #selector(ViewController.changeImage(timer:)),
-                             userInfo: nil, repeats: true)
-        
+        loadUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,7 +38,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @objc func changeImage(timer : Timer) {
+    fileprivate func loadUI() {
+        if let img = loadNextImage() {
+            self.image.image = img
+            
+            guard let prediction = predict(img) else {
+                return
+            }
+            
+            displayResult(prediction)
+        }
+        
+        
+    }
+    
+    fileprivate func loadNextImage() -> UIImage? {
         
         // get the image name by index
         let idx = (i % images.count)
@@ -50,27 +62,25 @@ class ViewController: UIViewController {
             self.image.image = nil
             self.fatalError(error: "Error occurred: Image not found!")
             i = 0
-            return
+            return nil
         }
         
-        self.image.image = img
-        predict(img)
-        
         i += 1
+        return img
     }
     
-    fileprivate func predict(_ image: UIImage) {
+    fileprivate func predict(_ image: UIImage) -> Inceptionv3Output? {
         
         guard let buffer = image.getCVPixelBuffer() else {
             fatalError(error: "Unkown error occurred!")
-            return
+            return nil
         }
         
-        if let prediction = try? model.prediction(image: buffer) {
-            let resultProb = getPrediction(prediction.classLabelProbs)
-            displayResult(resultProb)
+        guard let prediction = try? model.prediction(image: buffer) else {
+            return nil
         }
         
+        return prediction
     }
     
     fileprivate func getPrediction(_ prediction: [String: Double]) -> [Prediction] {
@@ -83,26 +93,47 @@ class ViewController: UIViewController {
         return result
     }
     
-    fileprivate func displayResult(_ predictionResult: [Prediction]) {
+    fileprivate func displayResult(_ prediction: Inceptionv3Output) {
         
-        var result = ""
-        for prediction in predictionResult {
-            
-            result += "Prediction: \(prediction.name)\n"
-            result += "Prob: \(prediction.probability.roundTo(places: 2))\n\n"
-        }
+        let headerText = "I think it's a \(prediction.classLabel)"
+        self.predictionResult = getPrediction(prediction.classLabelProbs)
         
         DispatchQueue.main.async() { [weak self] () -> Void in
-            self?.label.text = result
+            self?.headerLabel.text = headerText
+            self?.tableView.reloadData()
         }
     }
     
     fileprivate func fatalError(error: String) {
         if !error.isEmpty {
             DispatchQueue.main.async() { [weak self] () -> Void in
-                self?.label.text = error
+                self?.headerLabel.text = error
             }
         }
     }
 
+    @IBAction func nextBtnAction(_ sender: Any) {
+        loadUI()
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return predictionResult.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MLPredictionCell", for: indexPath)
+        
+        let predictionItem = predictionResult[indexPath.row]
+        cell.textLabel?.text = predictionItem.name
+        cell.detailTextLabel?.text = "hola everyone" //"\(predictionItem.probability.roundTo(places: 2))"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Top 5 Predictions"
+    }
 }
